63239315abb2200a738262898d19d1271404488c
RachelSaini06 <71180917+RachelSaini06@users.noreply.github.com>
2020-12-20 18:46:40 -0500

    Add files via upload

---
+++ Block4.js
@@ -0,0 +1,26 @@
+class Block4{
+  constructor(x, y, width, height, angle) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.1,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      //this.x = x;
+      //this.y = y;
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      rotate(angle);
+      fill("#6e41b4");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+}
---
+++ Block5.js
@@ -0,0 +1,26 @@
+class Block5{
+  constructor(x, y, width, height, angle) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.1,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      //this.x = x;
+      //this.y = y;
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      rotate(angle);
+      fill("#4330c0");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+}
